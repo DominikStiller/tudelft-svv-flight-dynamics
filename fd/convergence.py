e76aa7c10bc45b8ce72d8fd41ebf695a152bb4e9
joabron <101106300+joabron@users.noreply.github.com>
2023-03-13 09:41:14 +0100

    Add 4 unit conversion functions

--- fd/convergence.py
+++ fd/convergence.py
@@ -1,1 +1,16 @@
+import math
 
+def deg_to_rad(x):
+    """Convert value from degree to radians"""
+    return math.pi / 180 * x
+
+def lbshr_to_kgs(x):
+    """Convert value from lbs/hr to kg/s"""
+    return 0.45359237/3600 * x
+def psi_to_Pa(x):
+    """Convert value from psi to Pa"""
+    return 6894.757 * x
+
+def ftmin_to_ms(x):
+    """Convert value from ft/min to m/s"""
+    return 0.3048/60 * x
