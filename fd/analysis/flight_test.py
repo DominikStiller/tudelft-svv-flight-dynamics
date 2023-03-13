8922ae52f617dc66f80f5879dae977cee0594485
Dominik Stiller <domi.stiller@gmail.com>
2023-03-13 20:48:24 +0100

    Write initial version of FlightTest
    - Load data
    - Add mass timeseries caldulation

--- fd/analysis/flight_test.py
+++ fd/analysis/flight_test.py
@@ -1,30 +1,36 @@
 import pandas as pd
 
-from fd.analysis.aerodynamic_plots import plot_cl_alpha
 from fd.analysis.data_sheet import DataSheet
-from fd.analysis.ftis_measurements import FTISMeasurements
+from fd.analysis.ftis_measurements import process_ftis_measurements
+from fd.analysis.thrust import calculate_thrust_from_row
+from fd.io import load_ftis_measurements
+from fd.simulation import constants
 from fd.structs import AerodynamicParameters
 
 
 class FlightTest:
-    measurements: FTISMeasurements
+    df: pd.DataFrame
     data_sheet: DataSheet
 
     """Stores raw measurements, data sheet and estimated parameters"""
 
     def __init__(self, data_path: str):
         self._load(data_path)
+        self._add_derived_timeseries()
 
     def _load(self, data_path: str):
-        # Load measurements file and data sheet
-        # Add time-dependent columns
-        pass
+        self.df = process_ftis_measurements(load_ftis_measurements(data_path))
+        self.data_sheet = DataSheet(data_path)
 
-    def _add_mass_timeseries(self):
-        pass
-
-    def _add_thrust_timeseries(self):
-        pass
+    def _add_derived_timeseries(self):
+        self.df["time_min"] = self.df.index / 60
+        self.df["m"] = (
+            self.data_sheet.mass_initial - self.df["fuel_used_left"] - self.df["fuel_used_right"]
+        )
+        self.df["W"] = self.df["m"] * constants.g
+        # self.df["T_left"] = self.df.apply(calculate_thrust_from_row, axis=1)
+        # self.df["T_right"] = self.df.apply(calculate_thrust_from_row, axis=1)
+        # self.df["T"] = self.df["T_left"] + self.df["T_right"]
 
     def _add_reduced_velocity_timeseries(self):
         pass
@@ -35,7 +41,6 @@ class FlightTest:
     def analyze(self) -> AerodynamicParameters:
         # Estimate aerodynamic and eigenmotion parameters
         # Plot aerodynamic curves
-        plot_cl_alpha(self)
         pass
 
     def get_timeseries(self, column_name: str) -> pd.Series:
