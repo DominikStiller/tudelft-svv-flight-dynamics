ea3205e35608b2e37f13010aa91531b62ea2d206
tdekemp <t.dekemp@student.tudelft.nl>
2023-03-16 15:45:00 +0100

    Fixed docstrings

--- fd/analysis/aerodynamic_analysis.py
+++ fd/analysis/aerodynamic_analysis.py
@@ -4,14 +4,37 @@ import math
 import numpy as np
 
 def calc_stat_pres(hp):
+    """
+    Calculate the static pressure from the pressure height
+    Args:
+        hp (float): Pressure height[m]
 
+    Returns (float): The static pressure for the pressure height given[Pa]
+
+    """
     return constants.p0*(1 + constants.Tempgrad*hp/constants.Temp0)**(-constants.g/(constants.Tempgrad*constants.R))
 def calc_mach(hp, Vc):
+    """
 
+    Args:
+        hp (float): Pressure height[m]
+        Vc (float): The calibrated speed[m/s]
+
+    Returns (float): Mach number for the conditions given[-]
+
+    """
     return np.sqrt(2/(constants.gamma-1)*((1+constants.p0/calc_stat_pres(hp)*((1+(constants.gamma-1)/(2*constants.gamma)*constants.rho0/constants.p0*Vc**2)**(constants.gamma/(constants.gamma-1))-1))**((constants.gamma-1)/constants.gamma)-1))
 
 def calc_static_temp(Ttot, M):
+    """
 
+    Args:
+        Ttot (float): Total temperature[K]
+        M (float): Mach number[-]
+
+    Returns (float): Static temperature[K]
+
+    """
     return Ttot/(1 + (constants.gamma-1)/2*M**2)
 
 def calc_CL(W: float, V: float, S=constants.S, rho=constants.rho0) -> float:
