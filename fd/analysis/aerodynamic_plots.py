dec857b44a266ed63b9d1c6a3cdaa54ca56c8010
joabron <101106300+joabron@users.noreply.github.com>
2023-03-21 13:56:47 +0100

    Ad plots for elevator trim and control force curves

--- fd/analysis/aerodynamic_plots.py
+++ fd/analysis/aerodynamic_plots.py
@@ -52,3 +52,44 @@ def plot_cl_cd(CL, CD, CD0, e):
 
 
 # plot_cl_cd([0.5, 0.84, 0.29, 0.955], [0.031, 0.0532, 0.0240, 0.0630], 0.02, 0.8)
+
+# Elevator curves
+# [TODO: add smooth plots once data is known to see how it should be done]
+# [TODO: add fully controlled parameters to plots once these are know and its known how we want to do this]
+def plot_deltae_Ve(delta_e, V, xlabel_input = "V_e^*", ylabel_input = "δ_e^*"):
+    """
+    Plotting of the elevator trim curve. Depending on if V or V_e is used and the deflection or reduced deflection,
+    the non-reduced or reduced elevator trim curve plots can be obtained.
+
+    Args:
+        delta_e (array_like): Lift coefficient CL [deg]
+        V (array_like): airspeed [m/s]
+
+    Returns:
+        delta-V elevator trim curve plot
+    """
+    plt.scatter(V, delta_e, marker="x", color="black", s=50)
+    plt.xlabel(xlabel_input)
+    plt.ylabel(ylabel_input)
+
+    format_plot()
+    plt.show()
+
+def plot_Fe_Ve(F_e, V, xlabel_input = "V_e^*", ylabel_input = "F_e^*"):
+    """
+    Plotting of the elevator control force curve. Depending on if V or V_e is used and the F_e or reduced F_e,
+    the non-reduced or reduced elevator control force curve plots can be obtained.
+
+    Args:
+        F_e (array_like): Control stick force [N]
+        V (array_like): airspeed [m/s]
+
+    Returns:
+        F_e-V elevator control force curve plot
+    """
+    plt.scatter(V, F_e, marker="x", color="black", s=50)
+    plt.xlabel(xlabel_input)
+    plt.ylabel(ylabel_input)
+
+    format_plot()
+    plt.show()
