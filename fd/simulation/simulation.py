10e354e8cd0f5448c0780798ad1a059b4135838f
mfetecau <MFetecau@student.tudelft.nl>
2023-03-22 11:58:36 +0100

    Update simulation.py

--- fd/simulation/simulation.py
+++ fd/simulation/simulation.py
@@ -23,13 +23,15 @@ class Simulation:
         input = np.transpose(np.hstack((df_dutch_roll["delta_a"], df_dutch_roll['delta_r'])))
         t = np.arange(0, len(input)*0.1, 0.1)
         V0 = df_dutch_roll['tas'][0]
-        alpha0 = df_dutch_roll["alpha"][0]
+        beta0 = 0
+        phi0 = df_dutch_roll['phi'][0]
         theta0 = df_dutch_roll['theta'][0]
-        q0 = df_dutch_roll['q'][0]
+        p0 = df_dutch_roll['p'][0]
+        r0 = df_dutch_roll['r'][0]
         m = (df_dutch_roll['m'][0]+df_dutch_roll['m'][-1])/2
         rho0 = df_dutch_roll['rho'][0]
         CL = calc_CL(m*constants.g*np.cos(theta0), V0, rho0)
-        state0 = np.transpose(0, alpha0, theta0, q0)
+        state0 = np.transpose(beta0, phi0, p0, r0)
 
 
         model = AircraftModel(AerodynamicParameters)
@@ -44,13 +46,15 @@ class Simulation:
         input = np.transpose(np.hstack((df_dutch_roll_yd["delta_a"], df_dutch_roll_yd['delta_r'])))
         t = np.arange(0, len(input) * 0.1, 0.1)
         V0 = df_dutch_roll_yd['tas'][0]
-        alpha0 = df_dutch_roll_yd["alpha"][0]
+        beta0 = 0
+        phi0 = df_dutch_roll_yd['phi'][0]
         theta0 = df_dutch_roll_yd['theta'][0]
-        q0 = df_dutch_roll_yd['q'][0]
+        p0 = df_dutch_roll_yd['p'][0]
+        r0 = df_dutch_roll_yd['r'][0]
         m = (df_dutch_roll_yd['m'][0] + df_dutch_roll_yd['m'][-1]) / 2
         rho0 = df_dutch_roll_yd['rho'][0]
-        CL = calc_CL(m*constants.g*np.cos(theta0), V0, rho0)
-        state0 = np.transpose(0, alpha0, theta0, q0)
+        CL = calc_CL(m * constants.g * np.cos(theta0), V0, rho0)
+        state0 = np.transpose(beta0, phi0, p0, r0)
 
         model = AircraftModel(AerodynamicParameters)
         A, B, C, D = model.get_state_space_matrices_asymmetric(m, V0, rho0, theta0, CL)
@@ -64,13 +68,15 @@ class Simulation:
         input = np.transpose(np.hstack((df_spiral["delta_a"], df_spiral['delta_r'])))
         t = np.arange(0, len(input) * 0.1, 0.1)
         V0 = df_spiral['tas'][0]
-        alpha0 = df_spiral["alpha"][0]
+        beta0 = 0
+        phi0 = df_spiral['phi'][0]
         theta0 = df_spiral['theta'][0]
-        q0 = df_spiral['q'][0]
+        p0 = df_spiral['p'][0]
+        r0 = df_spiral['r'][0]
         m = (df_spiral['m'][0] + df_spiral['m'][-1]) / 2
         rho0 = df_spiral['rho'][0]
         CL = calc_CL(m * constants.g * np.cos(theta0), V0, rho0)
-        state0 = np.transpose(0, alpha0, theta0, q0)
+        state0 = np.transpose(beta0, phi0, p0, r0)
 
         model = AircraftModel(AerodynamicParameters)
         A, B, C, D = model.get_state_space_matrices_asymmetric(m, V0, rho0, theta0, CL)
@@ -84,13 +90,15 @@ class Simulation:
         input = np.transpose(np.hstack((df_aperiodic_roll["delta_a"], df_aperiodic_roll['delta_r'])))
         t = np.arange(0, len(input) * 0.1, 0.1)
         V0 = df_aperiodic_roll['tas'][0]
-        alpha0 = df_aperiodic_roll["alpha"][0]
+        beta0 = 0
+        phi0 = df_aperiodic_roll['phi'][0]
         theta0 = df_aperiodic_roll['theta'][0]
-        q0 = df_aperiodic_roll['q'][0]
+        p0 = df_aperiodic_roll['p'][0]
+        r0 = df_aperiodic_roll['r'][0]
         m = (df_aperiodic_roll['m'][0] + df_aperiodic_roll['m'][-1]) / 2
         rho0 = df_aperiodic_roll['rho'][0]
         CL = calc_CL(m * constants.g * np.cos(theta0), V0, rho0)
-        state0 = np.transpose(0, alpha0, theta0, q0)
+        state0 = np.transpose(beta0, phi0, p0, r0)
 
         model = AircraftModel(AerodynamicParameters)
         A, B, C, D = model.get_state_space_matrices_asymmetric(m, V0, rho0, theta0, CL)
@@ -99,6 +107,19 @@ class Simulation:
         result = np.hstack((t, np.transpose(yout)))
         df_result = pd.DataFrame(result, columns=['t', 'beta', 'phi', 'p', 'r'])
         return df_result
+
+    def simulate_phugoid(self, df_phugoid):
+        input = np.transpose(np.hstack((df_spiral["delta_a"], df_spiral['delta_r'])))
+        t = np.arange(0, len(input) * 0.1, 0.1)
+        V0 = df_spiral['tas'][0]
+        alpha0 = df_spiral["alpha"][0]
+        theta0 = df_spiral['theta'][0]
+        q0 = df_spiral['q'][0]
+        m = (df_spiral['m'][0] + df_spiral['m'][-1]) / 2
+        rho0 = df_spiral['rho'][0]
+        CL = calc_CL(m * constants.g * np.cos(theta0), V0, rho0)
+        state0 = np.transpose(0, alpha0, theta0, q0)
+
 
 
 
