4f8a8c6073f6c6e4cf7fb3af4621848edd99db99
mfetecau <MFetecau@student.tudelft.nl>
2023-03-27 12:03:03 +0200

    .

--- fd/simulation/simulation.py
+++ fd/simulation/simulation.py
@@ -200,8 +200,8 @@ if __name__ == "__main__":
             )
         )
     )
-    df = FlightTest("data/B24").df_spiral
-    df_out = sim.simulate_spiral(df)
+    df = FlightTest("data/B24").df_dutch_roll
+    df_out = sim.simulate_dutch_roll(df)
     fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1)
     """
     y1 = "tas"
@@ -220,7 +220,7 @@ if __name__ == "__main__":
     ax1.set_ylabel(y1)
     ax2.plot(df_out.index, df_out[y2])
     ax2.plot(df_out.index, df[y2], color="black")
-    #ax2.set_ylim(-0.6, 0.3)
+    #ax2.set_ylim(-0.2, 2.7)
     ax2.set_ylabel(y2)
     ax3.plot(df_out.index, df_out[y3])
     ax3.plot(df_out.index, df[y3], color="black")
