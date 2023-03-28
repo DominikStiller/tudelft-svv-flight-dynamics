05bfccfac8949121d32af4ceaffbdfb86e03596f
Dominik Stiller <domi.stiller@gmail.com>
2023-03-28 18:56:36 +0200

    Add idealized eigenvalues to comparison

--- fd/validation/comparison_eigenvalues.py
+++ fd/validation/comparison_eigenvalues.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+from fd.analysis.aerodynamics import calc_CL
 from fd.analysis.flight_test import FlightTest
 from fd.simulation.aircraft_model import AircraftModel
 from fd.validation.eigenmotion_characteristics import (
@@ -22,51 +25,79 @@ class EigenvalueComparison:
         self._compare_spiral()
 
     def _compare_phugoid(self):
-        A, _, _, _ = self.model.get_state_space_matrices_symmetric_from_df(
-            self.flight_test.df_phugoid
-        )
+        data = self.flight_test.df_phugoid
+        A, _, _, _ = self.model.get_state_space_matrices_symmetric_from_df(data)
         eigs, _ = self.model.get_eigenvalues_and_eigenvectors(A)
         eig_phugoid = eigs[2]
-        V0 = self.flight_test.df_phugoid["tas"].iloc[0]
-        P, T_half = characteristics_phugoid(eig_phugoid, V0)
-        print(f"Phugoid: P = {P:.2} s, T_half = {T_half:.2} s")
+        P, T_half = characteristics_phugoid(eig_phugoid)
+        print(f"Phugoid (simulated): P = {P:.3f} s, T_half = {T_half:.3f} s")
+
+        m = (data["m"].iloc[0] + data["m"].iloc[-1]) / 2
+        V0 = data["tas"].iloc[0]
+        rho0 = data["rho"].iloc[0]
+        theta0 = data["theta"].iloc[0]
+        eig_phugoid = self.model.get_idealized_phugoid_eigenvalues(m, rho0, theta0, V0)[0]
+        P, T_half = characteristics_phugoid(eig_phugoid)
+        print(f"Phugoid (idealized): P = {P:.3f} s, T_half = {T_half:.3f} s")
 
     def _compare_short_period(self):
-        A, _, _, _ = self.model.get_state_space_matrices_symmetric_from_df(
-            self.flight_test.df_short_period
-        )
+        data = self.flight_test.df_short_period
+        A, _, _, _ = self.model.get_state_space_matrices_symmetric_from_df(data)
         eigs, _ = self.model.get_eigenvalues_and_eigenvectors(A)
         eig_short_period = eigs[0]
-        V0 = self.flight_test.df_short_period["tas"].iloc[0]
-        P, T_half = characteristics_short_period(eig_short_period, V0)
-        print(f"Short period: P = {P:.2} s, T_half = {T_half:.2} s")
+        P, T_half = characteristics_short_period(eig_short_period)
+        print(f"Short period (simulated): P = {P:.3f} s, T_half = {T_half:.3f} s")
+
+        m = (data["m"].iloc[0] + data["m"].iloc[-1]) / 2
+        V0 = data["tas"].iloc[0]
+        rho0 = data["rho"].iloc[0]
+        eig_short_period = self.model.get_idealized_shortperiod_eigenvalues(m, rho0, V0)[0]
+        P, T_half = characteristics_short_period(eig_short_period)
+        print(f"Short period (idealized): P = {P:.3f} s, T_half = {T_half:.3f} s")
 
     def _compare_dutch_roll(self):
-        A, _, _, _ = self.model.get_state_space_matrices_asymmetric_from_df(
-            self.flight_test.df_dutch_roll
-        )
+        data = self.flight_test.df_dutch_roll
+        A, _, _, _ = self.model.get_state_space_matrices_asymmetric_from_df(data)
         eigs, _ = self.model.get_eigenvalues_and_eigenvectors(A)
         eig_dutch_roll = eigs[1]
-        V0 = self.flight_test.df_dutch_roll["tas"].iloc[0]
-        P, T_half = characteristics_dutch_roll(eig_dutch_roll, V0)
-        print(f"Dutch roll: P = {P:.2} s, T_half = {T_half:.2} s")
+        P, T_half = characteristics_dutch_roll(eig_dutch_roll)
+        print(f"Dutch roll (simulated): P = {P:.3f} s, T_half = {T_half:.3f} s")
+
+        m = (data["m"].iloc[0] + data["m"].iloc[-1]) / 2
+        V0 = data["tas"].iloc[0]
+        rho0 = data["rho"].iloc[0]
+        eig_dutch_roll = self.model.get_idealized_dutchroll_eigenvalues(m, rho0, V0)[0]
+        P, T_half = characteristics_dutch_roll(eig_dutch_roll)
+        print(f"Dutch roll (idealized): P = {P:.3f} s, T_half = {T_half:.3f} s")
 
     def _compare_aperiodic_roll(self):
-        A, _, _, _ = self.model.get_state_space_matrices_asymmetric_from_df(
-            self.flight_test.df_aperiodic_roll
-        )
+        data = self.flight_test.df_aperiodic_roll
+        A, _, _, _ = self.model.get_state_space_matrices_asymmetric_from_df(data)
         eigs, _ = self.model.get_eigenvalues_and_eigenvectors(A)
         eig_aperiodic_roll = eigs[0]
-        V0 = self.flight_test.df_aperiodic_roll["tas"].iloc[0]
-        tau = time_constant_aperiodic_roll(eig_aperiodic_roll, V0)
-        print(f"Aperiodic roll: tau = {tau:.2}")
+        tau = time_constant_aperiodic_roll(eig_aperiodic_roll)
+        print(f"Aperiodic roll (simulated): tau = {tau:.3f}")
+
+        m = (data["m"].iloc[0] + data["m"].iloc[-1]) / 2
+        V0 = data["tas"].iloc[0]
+        rho0 = data["rho"].iloc[0]
+        eig_aperiodic_roll = self.model.get_idealized_aperiodicroll_eigenvalues(m, rho0, V0)
+        tau = time_constant_aperiodic_roll(eig_aperiodic_roll)
+        print(f"Aperiodic roll (idealized): tau = {tau:.3f}")
 
     def _compare_spiral(self):
-        A, _, _, _ = self.model.get_state_space_matrices_asymmetric_from_df(
-            self.flight_test.df_spiral
-        )
+        data = self.flight_test.df_spiral
+        A, _, _, _ = self.model.get_state_space_matrices_asymmetric_from_df(data)
         eigs, _ = self.model.get_eigenvalues_and_eigenvectors(A)
-        V0 = self.flight_test.df_spiral["tas"].iloc[0]
         eig_spiral = eigs[3]
-        tau = time_constant_spiral(eig_spiral, V0)
-        print(f"Spiral: tau = {tau:.2}")
+        tau = time_constant_spiral(eig_spiral)
+        print(f"Spiral (simulated): tau = {tau:.3f}")
+
+        m = (data["m"].iloc[0] + data["m"].iloc[-1]) / 2
+        V0 = data["tas"].iloc[0]
+        rho0 = data["rho"].iloc[0]
+        theta0 = data["theta"].iloc[0]
+        CL = calc_CL(data["W"].iloc[0] * np.cos(theta0), V0, rho0)
+        eig_aperiodic_roll = self.model.get_idealized_spiral_eigenvalues(m, rho0, V0, CL)
+        tau = time_constant_spiral(eig_aperiodic_roll)
+        print(f"Spiral (idealized): tau = {tau:.3f}")
